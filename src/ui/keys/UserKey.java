a2b90fcc501a6f3a6cc7dc6dbeae1edf43c7b384
bqdx127@gmail.com <bqdx127@gmail.com@76af6180-b6b4-5a94-7851-e1646efe27f6>
2011-09-14 15:01:39 +0000

    *добавлено немного действий *небольшая чистка комментариев

--- src/ui/keys/UserKey.java
+++ src/ui/keys/UserKey.java
@@ -113,30 +113,6 @@ public class UserKey extends IconTextElement {
             outputStream.writeBoolean(secLong);
         } catch (IOException e) { }
     }
- /*
-    public static final String[] COMMANDS_DESC = {
-            SR.get(SR.MS_NO)
-            ,SR.get(SR.MS_OPTIONS)
-            ,SR.get(SR.MS_CLEAN_ALL_MESSAGES)
-            ,SR.get(SR.MS_RECONNECT)
-            ,SR.get(SR.MS_STATS)
-            ,SR.get(SR.MS_STATUS)
-            ,SR.get(SR.MS_FILE_TRANSFERS)
-            ,SR.get(SR.MS_ARCHIVE)
-            ,SR.get(SR.MS_DISCO)
-            ,SR.get(SR.MS_PRIVACY_LISTS)
-            ,SR.get(SR.MS_CUSTOM_KEYS)
-            ,SR.get(SR.MS_CLEAR_POPUPS)
-            ,SR.get(SR.MS_APP_MINIMIZE)
-            ,SR.get(SR.MS_INVERT)
-            ,SR.get(SR.MS_FULLSCREEN)
-//#ifdef XML_CONSOLE
-//#             ,SR.get(SR.MS_XML_CONSOLE)
-//#endif
-    };
-
-  *
-  */
 
     public static String getKeyName(int keyCode){
         switch( keyCode){

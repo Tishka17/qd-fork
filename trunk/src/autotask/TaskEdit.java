394942ba8aa6d212aa9943d368f3ba174366abfc
bqdx127@gmail.com <bqdx127@gmail.com@76af6180-b6b4-5a94-7851-e1646efe27f6>
2011-09-18 18:01:33 +0000

    ! исправления локали для планировщика (en/ru)

--- trunk/src/autotask/TaskEdit.java
+++ trunk/src/autotask/TaskEdit.java
@@ -60,39 +60,39 @@ public class TaskEdit extends DefForm {
 	if( newTask) te=new TaskElement();
 	this.te= te;
 
-        atName= new TextInput( "Имя задачи", te.Name, TextField.ANY);
+        atName= new TextInput( SR.get(SR.MS_SHED_NAME), te.Name, TextField.ANY);
 
-        atType= new DropChoiceBox( "Тип задания");
+        atType= new DropChoiceBox( SR.get(SR.MS_SHED_TYPE));
             atType.append(new IconTextElement(SR.get(SR.MS_DISABLED), RosterIcons.getInstance(), RosterIcons.ICON_PLUGINBOX_UNCHECKED));
             atType.append(new IconTextElement(SR.get(SR.MS_BY_TIME_), ActionsIcons.getInstance(), ActionsIcons.ICON_TIME));
             atType.append(new IconTextElement(SR.get(SR.MS_BY_TIMER_), ActionsIcons.getInstance(), ActionsIcons.ICON_TIME));
         atType.setSelectedIndex( te.Type);
 
-        atAction= new DropChoiceBox( "Действие");
+        atAction= new DropChoiceBox( SR.get(SR.MS_SHED_ACTION));
             //atAction.items= UserActions.getActionsList( UserActions.UA_TASKS);
             // не правильно работает ???
-            atAction.append( new IconTextElement( "Напоминалка", ActionsIcons.getInstance(), ActionsIcons.ICON_TIME));
+            atAction.append( new IconTextElement( SR.get(SR.MS_SHED_REMINDER), ActionsIcons.getInstance(), ActionsIcons.ICON_TIME));
             // нулевой элемент в планировщике - напоминалка (обрабатываем отдельно)
             Vector al= UserActions.getActionsList( UserActions.UA_TASKS);
             for( int i=1; i <al.size(); i++)
                 atAction.append( al.elementAt( i));
         atAction.setSelectedIndex( te.Action);
 
-        atOnce= new CheckBox("Выполнить однажды", te.Once);
+        atOnce= new CheckBox( SR.get(SR.MS_SHED_EONCE), te.Once);
 
-        atNbox= new CheckBox( "Уведомления", te.Notify());
-            atVbox= new CheckBox( "Вибрацией", te.NotifyV());
-            atLbox= new CheckBox( "Миганием", te.NotifyL());
-            atSbox= new CheckBox( "Гудком", te.NotifyS());
+        atNbox= new CheckBox( SR.get(SR.MS_SHED_NOTIFICATION), te.Notify());
+            atVbox= new CheckBox( SR.get(SR.MS_SHED_VIBRA), te.NotifyV());
+            atLbox= new CheckBox( SR.get(SR.MS_SHED_LIGHT), te.NotifyL());
+            atSbox= new CheckBox(SR.get(SR.MS_SHED_SOUND), te.NotifyS());
 
         atTimeDesc=new SimpleString(SR.get(SR.MS_AUTOTASK_TIME), true);
 
         atHour=new NumberInput(SR.get(SR.MS_AUTOTASK_HOUR), te.Hour, 0, 23);
         atMin=new NumberInput(SR.get(SR.MS_AUTOTASK_MIN), te.Minute, 0, 59);
         atDelay=new NumberInput(SR.get(SR.MS_AUTOTASK_DELAY), te.Timer, 1, 600);
-        atNotifyD=new NumberInput( "Упреждение (мин)", te.NotifyD, 1, 15);
+        atNotifyD=new NumberInput( SR.get(SR.MS_SHED_PREEMPTION), te.NotifyD, 1, 15);
 
-        atText=new TextInput( "Текст уведомления", te.Text, TextField.ANY);
+        atText=new TextInput( SR.get(SR.MS_SHED_TEXT), te.Text, TextField.ANY);
     }
 
     public void cmdOk() {
